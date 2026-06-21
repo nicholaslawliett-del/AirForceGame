072668a7c7e24ac8142fcaa3c21642454a37f7cb
SoftnWet <nicholaslawliett@gmail.com>
2026-06-21 17:25:18 +0700

    idk

--- src/airforce/core/Main.java
+++ src/airforce/core/Main.java
@@ -1,22 +1,22 @@
+package airforce.core;
+
 import javax.swing.JFrame;
 
+/**
+ * Entry point program. Jalankan file ini untuk membuka game.
+ */
 public class Main {
     public static void main(String[] args) {
-        // 1. Membuat jendela game
-        JFrame window = new JFrame();
+        JFrame window = new JFrame("AirForce Shooter");
+        GameManager gameManager = new GameManager();
+
+        window.add(gameManager);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("Winger Shooter Duar Duar");
-
-        // 2. Memanggil dan memasukkan GameManager ke dalam jendela
-        GameManager gameManager = new GameManager();
-        window.add(gameManager);
-
-        window.pack(); // Menyesuaikan ukuran jendela dengan GameManager (800x600)
-        window.setLocationRelativeTo(null); // Jendela otomatis di tengah layar
+        window.pack();
+        window.setLocationRelativeTo(null);
         window.setVisible(true);
 
-        // 3. Menjalankan Jantung Game (Game Loop)
-        gameManager.startGameThread();
+        gameManager.startGame();
     }
 }
